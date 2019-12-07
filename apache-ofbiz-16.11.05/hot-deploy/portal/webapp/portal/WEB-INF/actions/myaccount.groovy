5ac238f9c2397270f9e710e5960a5a29fc216b9c
Harish <harishbn@arithaconsulting.com>
2019-12-07 12:32:03 +0530

    myaccount page implementation

---
+++ apache-ofbiz-16.11.05/hot-deploy/portal/webapp/portal/WEB-INF/actions/myaccount.groovy
@@ -0,0 +1,10 @@
+import org.apache.ofbiz.base.util.UtilMisc
+
+// Get person data from Database
+Map inputs = UtilMisc.toMap("partyId", userLogin.partyId)
+person = delegator.findOne("Person", inputs, false)
+
+context.person = person;
+context.email = userLogin.userLoginId
+
+// TODO: Fetch user role
